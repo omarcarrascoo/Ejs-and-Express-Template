5686318cc2b2ec3c88ca43a11d9893788e85d9bd
Omar Carrasco <omar.carrsaco15.16@gmail.com>
2022-08-31 21:39:55 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas){
+      this.nombre=nombre;
+      this.apellido=apellido;
+      this.libros=[];
+      this.mascotas=[]
+    }
+  }
+console.log("hola mundo")
