c93192d474d6165ee0d009f1a33678ceace7ff85
Omar Carrasco <omar.carrsaco15.16@gmail.com>
2022-09-02 10:16:40 -0500

    reentrega 1

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 class Usuario {
-    constructor(nombre, apellido, libros, mascotas){
-      this.nombre=nombre;
-      this.apellido=apellido;
-      this.libros=[];
-      this.mascotas=[]
-    }
+  constructor(nombre, apellido, libros, mascotas){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.libros=libros;
+    this.mascotas=mascotas;
   }
-console.log("hola mundo")
+}
+
+const user1 = 
+    new Usuario (
+      'Carlos', 
+      'Perez',
+      [
+        {nombre: "Asi hablo Zaratustra", autor: "Nietzsche"},
+        {nombre: "21 lecciones para el siglo XXI", autor: "Yuval Noah Harari"}
+      ], 
+      ["Tortuga", "Gato", "Perro"])
+
+
+
+function getFullName(user){
+console.log(`${user.nombre} ${user.apellido}`)
+}
+function addMascota(user, mascota){
+console.log("/////Mascotas////////")
+console.log(user.mascotas)
+user.mascotas.push(mascota)
+console.log(user.mascotas)
+}
+const countMascotas=(user)=>{
+console.log(`Numero de mascotas: ${user.mascotas.length}`)
+}
+const getBookNames=(user)=>{
+console.log("/////LIBROS////////") 
+user.libros.forEach(obj => console.log(obj.nombre))
+
+}
+const addBook=(user, libro, autor)=>{
+obj = {nombre: libro, autor:autor};
+user.libros.push(obj);
+
+}
+getFullName(user1)
+addMascota(user1, "Frida")
+countMascotas(user1)
+addBook(user1, "De animales a dioses", "Yuval Noah")
+getBookNames(user1)
