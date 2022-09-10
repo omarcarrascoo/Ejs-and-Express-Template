130c83395b9a5b3fcd307ca618eeae9be2bae801
Omar Carrasco <omar.carrsaco15.16@gmail.com>
2022-09-10 11:58:31 -0500

    Entrega 2

--- index.js
+++ index.js
@@ -1,48 +1,70 @@
-class Usuario {
-  constructor(nombre, apellido, libros, mascotas){
-    this.nombre=nombre;
-    this.apellido=apellido;
-    this.libros=libros;
-    this.mascotas=mascotas;
+const fs = require('fs')
+
+class Contenedor {
+  constructor(rutaArchivo){
+    this.rutaArchivo = rutaArchivo
   }
-}
 
-const user1 = 
-    new Usuario (
-      'Carlos', 
-      'Perez',
-      [
-        {nombre: "Asi hablo Zaratustra", autor: "Nietzsche"},
-        {nombre: "21 lecciones para el siglo XXI", autor: "Yuval Noah Harari"}
-      ], 
-      ["Tortuga", "Gato", "Perro"])
+  async #leerUnArchivo(){
+    try{
+      const contenido = await fs.promises.readFile(this.rutaArchivo, 'utf-8')
+      const contenidoParseado = JSON.parse(contenido)
+      return contenidoParseado
+    } catch (error){
+      console.log(error)
+    }
+  }
+  async save (obj){
+    const contenidoArchivo = await this.#leerUnArchivo()
+    if (contenidoArchivo.length !==0 ){
+      await fs.promises.writeFile(this.rutaArchivo,JSON.stringify([...contenidoArchivo, {...obj, id: contenidoArchivo[contenidoArchivo.length - 1].id + 1}],null,2), 'utf-8')
+    }
+    else {
+      await fs.promises.writeFile(this.rutaArchivo, JSON.stringify([{...obj, id:1}]))
+    }
+  }
+
+  async getAll(){
+    const contenidoArchivo = await this.#leerUnArchivo()
+    console.log(contenidoArchivo)
+  }
+
+  async getById(id){
+    const contenidoArchivo = await this.#leerUnArchivo()
+    contenidoArchivo.find(object =>{
+      if(object.id === id){
+          console.log(object);
+      }
+  });
+  }
+  async delateById(id){
+    const contenidoArchivo = await this.#leerUnArchivo()
+    const indexOfObject = contenidoArchivo.findIndex(object => {
+      return object.id === id;
+    });
+    if (id == id) {
+      contenidoArchivo.splice(indexOfObject, 1)
+      await fs.promises.writeFile(this.rutaArchivo,JSON.stringify([...contenidoArchivo],null,2), 'utf-8')
+      console.log(contenidoArchivo)
+
+    }else{
+      console.log("No se encontro el producto")
+    }
+    
+  }
+
+  async delateAll (){
+    await fs.promises.unlink(this.rutaArchivo)
+  }
+
+}
+const contenedor = new Contenedor('./productos.txt')
+contenedor.save({producto: 'Cafe', precio: 100 })
+contenedor.getById(3)
+contenedor.getAll()
+// contenedor.delateById(3)
+// contenedor.delateAll()
 
 
 
-function getFullName(user){
-console.log(`${user.nombre} ${user.apellido}`)
-}
-function addMascota(user, mascota){
-console.log("/////Mascotas////////")
-console.log(user.mascotas)
-user.mascotas.push(mascota)
-console.log(user.mascotas)
-}
-const countMascotas=(user)=>{
-console.log(`Numero de mascotas: ${user.mascotas.length}`)
-}
-const getBookNames=(user)=>{
-console.log("/////LIBROS////////") 
-user.libros.forEach(obj => console.log(obj.nombre))
 
-}
-const addBook=(user, libro, autor)=>{
-obj = {nombre: libro, autor:autor};
-user.libros.push(obj);
-
-}
-getFullName(user1)
-addMascota(user1, "Frida")
-countMascotas(user1)
-addBook(user1, "De animales a dioses", "Yuval Noah")
-getBookNames(user1)
